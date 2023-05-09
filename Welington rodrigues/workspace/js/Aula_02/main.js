54c50c7638ace6cc8d15b5b4675fb25924286a2b
Limagio <giovanamflima@gmail.com>
2023-05-08 21:48:38 -0300

    Aula 02 - JavaScript

---
+++ Welington rodrigues/workspace/js/Aula_02/main.js
@@ -0,0 +1,57 @@
+//NaN
+
+var zero = 10 + 21;
+
+console.log(zero);
+
+var zero2 = "500" - "190";
+
+console.log(zero2);
+
+//contanezização
+
+var texto1 = "welington";
+
+var texto2 = "rodrigues";
+
+console.log(texto1 + texto2);
+
+console.clear();
+
+//operadores aritméticos
+var ovo1 = 5;
+var ovo2 = 4;
+
+//Adição
+var adicao = ovo1 + ovo2;
+console.log("resultado da conta é: " + adicao);
+
+//Subtração
+var Subtracao = ovo1 - ovo2;
+console.log("resultado da conta é: " + Subtracao);
+
+//divisão
+var divisao = ovo1 / ovo2;
+console.log("resultado da conta é: " + divisao);
+
+//multiplicação
+var multiplicacao = ovo1 * ovo2;
+console.log(multiplicacao + " - este é o resultado da conta");
+
+console.clear()
+//precedencia de operadores
+//() / * + -
+
+var numero3 = (5+3) / 4 + 6
+console.log(numero3)
+
+//comparação
+var compara = ovo1 < ovo2
+console.log(compara) //false
+
+//terceiro tipo de comparação
+var variavel1 = 3
+var variavel2 = "3"
+var compara2 = variavel1 === variavel2 // false 
+console.log(compara2)
+
